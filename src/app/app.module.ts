9b6d7344f30b625f35cdce5e829cb7a2b14600ec
DavidGasparyan <davidgasparyang@gmail.com>
2021-09-23 23:05:53 +0400

    Generated small project architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './board/list/list.component';
+import { CardComponent } from './board/list/card/card.component';
+import { CardDetailComponent } from './board/list/card/card-detail/card-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ListComponent,
+    CardComponent,
+    CardDetailComponent
   ],
   imports: [
     BrowserModule,
