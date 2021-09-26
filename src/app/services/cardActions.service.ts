c01e6028cd6c2fde9d53b03461909e7323fea83b
DavidGasparyan <davidgasparyang@gmail.com>
2021-09-26 13:35:05 +0400

    Fixed card deletion bug

--- src/app/services/cardActions.service.ts
+++ src/app/services/cardActions.service.ts
@@ -7,24 +7,25 @@ export class CardActionsService {
 
   @Output() cardDeletionEvent: EventEmitter<Card[]> = new EventEmitter<Card[]>();
 
-  cards:Card[] = [new Card('To do first card', 'Desc', [],  Date.now())];
-  constructor() { };
+  cards: Card[] = [new Card('To do first card', 'Desc', [], Date.now())];
 
-  cardCreate(cardName:string) {
+  constructor () { };
+
+  cardCreate (cardName: string) {
     if (cardName) {
       this.cards.push(new Card(cardName, 'Desc', [], Date.now()));
       cardName = '';
     }
   };
 
-  cardDelete(index: number) {
-    
+  cardDelete (index: number) {
     this.cards = this.cards.filter((cardItem, cardIndex) => {
-      return index !== cardIndex
+      return index === cardIndex
     });
     this.cardDeletionEvent.emit(this.cards)
   };
-  get cardData() {
+
+  get cardData () {
     return this.cards
   }
 }
